6a41a8fe0476dfd180a55e9a2589730628588d3d
sergey.volodzko <sergey.volodzko@1000geeks.com>
2019-06-26 21:13:11 +0300

    Added style.

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .views import index, by_rubric
+from .views import BbCreateView
+
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', index),
+    path('add/', BbCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 
 from .models import Bb, Rubric
 
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .forms import BbForm
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
