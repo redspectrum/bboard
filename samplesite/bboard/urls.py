b61cf60668cb6c586f467486ec7204956fb812f1
sergey.volodzko <sergey.volodzko@1000geeks.com>
2019-07-05 20:54:08 +0300

    Added new Function

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import index, by_rubric
 from .views import BbCreateView
 
+from .views import *
 
 urlpatterns = [
-    path('add/', BbCreateView.as_view(), name='add'),
+    path('add/', add_and_save, name='add'),
+    # path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
 
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Bb, Rubric
 
@@ -7,7 +9,9 @@ from django.urls import reverse_lazy
 
 from .forms import BbForm
 
+
 class BbCreateView(CreateView):
+
     template_name = 'bboard/create.html'
     form_class = BbForm
     success_url = reverse_lazy('index')
@@ -18,8 +22,8 @@ class BbCreateView(CreateView):
         return context
 
 
-
 def index(request):
+
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
 
@@ -35,8 +39,23 @@ def by_rubric(request, rubric_id):
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {
-        'bbs':bbs,
+        'bbs': bbs,
         'rubrics': rubrics,
         'current_rubric': current_rubric
     }
     return render(request, 'bboard/by_rubric.html', context)
+
+def add_and_save(request):
+    if request.method == 'POST':
+        bbf = BbForm(request.POST)
+        if bbf.is_valid():
+            bbf.save()
+            return HttpResponseRedirect(reverse('by_rubric',
+                                                kwargs={'rubric_id': bbf.cleaned_data('rubric').pk}))
+        else:
+            context = {'form': bbf}
+            return render(request, 'bboard/create.html', context)
+    else:
+        bbf = BbForm()
+        context = {'form': bbf}
+        return render(request, 'bboard/create.html', context)
